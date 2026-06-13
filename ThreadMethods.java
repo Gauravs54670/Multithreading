380512c4f2a34f337ee5cba6beebc4d3f249c381
Gaurav <gauravsoni54670@gmail.com>
2026-06-13 18:03:04 +0530

    Thread.join() method

---
+++ ThreadMethods.java
@@ -0,0 +1,21 @@
+public class ThreadMethods {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Main Thread execution starts...");
+        Runnable task = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " started execution");
+            try {
+                Thread.sleep(3000);
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        };
+        Thread t1 = new Thread(task, "Execution 1");
+        Thread t2 = new Thread(task, "Execution 2");
+        t1.start();
+        t1.join();
+        t2.start();
+        System.out.println("Main thread execution completed.");
+    }
+}
