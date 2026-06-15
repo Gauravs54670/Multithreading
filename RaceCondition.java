8a2ce14a411a799b8f41d0a9c4617f633d80e47a
Gaurav <gauravsoni54670@gmail.com>
2026-06-15 21:46:37 +0530

    commit

--- RaceCondition.java
+++ RaceCondition.java
@@ -1,28 +1,24 @@
-class Counter {
-    public int counter = 0;
-    public void increment() {
-        counter++;
-    }
-}
 public class RaceCondition {
     public static void main(String[] args) {
         Counter counter = new Counter();
         Runnable task = () -> {
-            for(int i = 1; i<=100000; i++){
+            for(int i = 1; i<=1000; i++)
                 counter.increment();
-            }
         };
         Thread t1 = new Thread(task);
         Thread t2 = new Thread(task);
-        t1.start();
-        t2.start();
+        t1.start(); t2.start();
         try {
-            t1.join();
-            t2.join();
+            t1.join(); t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
-        System.out.println(counter.counter);
+        System.out.println("Count of counter is " + Counter.counter);
+    }
+}
+class Counter {
+    public static int counter = 0;
+    public void increment() {
+        counter += 1;
     }
 }
